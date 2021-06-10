5a6e696da2ab13610f82b811fa759c22a7cd0313
Amar Bansal <amarbansal28@gmail.com>
2021-06-11 01:00:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 function secondSmallest(arr){
     arr = [...new Set(arr)];
     arr = arr.sort(function(a,b){return a-b});
-    return arr[arr.length-2];
+    return arr[1];
 }
 
 console.log(secondSmallest([4,5,9,1,2,4,0,3,5,7,1]))
+
+
+function secondSmallest_2(arr){
+    let obj={};
+    for(let a in arr){
+        obj[a]={};
+    }
+    arr = Object.keys(obj);
+    let first = Number.MAX_VALUE;
+    let second  = Number.MAX_VALUE;
+    for(let i =0; i< arr.length; i++){
+        if(first < arr[i]){
+            second = first;
+            first = arr[i];
+        }else if(second < arr[i] && first != second){
+            second = arr[i];
+        }
+    }
+    return second;
+}
+
+console.log(secondSmallest_2([4,5,9,1,2,4,0,3,5,7,1]))
